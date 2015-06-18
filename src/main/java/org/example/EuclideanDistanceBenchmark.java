f2fbd1c0ed2a52fdbfc4e402e478dfb0424ffd1e
Manish Baxi <baxi.manish@welue.com>
2015-06-19 01:26:09 +0530

    Initial commit.

---
+++ src/main/java/org/example/EuclideanDistanceBenchmark.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.example;
+
+import org.openjdk.jmh.annotations.*;
+
+public class EuclideanDistanceBenchmark {
+  @Benchmark
+  @Fork(0)
+  @Measurement(batchSize = 1000, iterations = 10)
+  @Warmup(iterations = 10)
+  public void run() {
+    distance(new Point(Math.random(), Math.random()), new Point(Math.random(), Math.random()));
+  }
+
+  private double distance(Point a, Point b) {
+    double dx = a.getX() - b.getX();
+    double dy = a.getY() - b.getY();
+
+    return Math.sqrt(dx * dx - dy * dy);
+  }
+}
+
+class Point {
+  private final double x;
+  private final double y;
+
+  Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+}
